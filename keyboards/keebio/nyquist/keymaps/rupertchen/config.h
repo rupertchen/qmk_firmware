e96325acb8dc958b88d39ea7f50ec1d37cc4cde8
Rupert Chen <rupert.chen@gmail.com>
2023-09-04 22:12:55 -0700

    Document LED indexes

--- keyboards/keebio/nyquist/keymaps/rupertchen/config.h
+++ keyboards/keebio/nyquist/keymaps/rupertchen/config.h
@@ -3,4 +3,16 @@
 // The LED map is not intuitive.  The i-th entry of the map defines the
 // position to read out of the LED buffer.  I.e., multiple entries may map to
 // the same LED buffer position.
+
+// Nyquist LED legend
+//
+// Left, 0-5
+// 2 1 0
+// 3 4 5
+//
+// Right, 6-B
+// b a 9
+// 6 7 8
+//
 #define RGBLIGHT_LED_MAP { 5, 0, 1, 2, 3, 4, 7, 8, 9, 10, 11, 6 }
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
