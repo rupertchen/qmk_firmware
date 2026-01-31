82236a5777dd2460065e37ad16453ae276600feb
Rupert Chen <rupert.chen@gmail.com>
2026-01-31 15:37:38 -0800

    Port Voyager keymap

--- keyboards/keebio/nyquist/keymaps/rupertchen/keymap.c
+++ keyboards/keebio/nyquist/keymaps/rupertchen/keymap.c
@@ -39,26 +39,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_ortho_5x12(
-  KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
-  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
-  KC_ESC, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
-  _______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, _______,
-  KC_LCTL, KC_GRV, KC_CAPS, KC_LALT, KC_BSPC, KC_DEL, LT(_LOWER,KC_ENT), KC_SPC, KC_RGUI, KC_LBRC, KC_RBRC, MO(_RAISE)
+  KC_EQL,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,              KC_6,              KC_7,         KC_8,         KC_9,         KC_0,            KC_MINS,
+  KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,              KC_Y,              KC_U,         KC_I,         KC_O,         KC_P,            KC_BSLS,
+  KC_ESC,  LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,              KC_H,              RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
+  _______, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,              KC_N,              KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         _______,
+  KC_LCTL, KC_GRV,       KC_CAPS,      KC_LALT,      KC_BSPC,      LT(_RAISE,KC_DEL), LT(_LOWER,KC_ENT), KC_SPC,       KC_RGUI,      KC_LBRC,      KC_RBRC,         MO(_ADJUST)
 ),
 
 /* Lower
  */
 [_LOWER] = LAYOUT_ortho_5x12(
-  KC_F1,   KC_F2,       KC_F3,      KC_F4,         KC_F5,   KC_F6,   KC_F7,   KC_F8,      KC_F9,      KC_F10,  KC_F11, KC_F12,
-  KC_NO,   KC_MS_WH_UP, KC_MS_UP,   KC_MS_WH_DOWN, KC_NO,   KC_NO,   KC_HOME, KC_PGDN,    KC_PGUP,    KC_END,  KC_NO,  KC_PSCR,
-  _______, KC_MS_LEFT,  KC_MS_DOWN, KC_MS_RIGHT,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN,    KC_UP,      KC_RGHT, KC_NO,  KC_NO,
-  _______, KC_NO,       KC_NO,      KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_MS_BTN1, KC_MS_BTN2, KC_NO,   KC_NO,  _______,
-  _______, KC_NO,       KC_NO,      _______,       _______, _______, _______, _______,    _______,    KC_NO,   KC_NO,  _______
+  KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_NO, KC_NO,
+  KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO, KC_PSCR,
+  KC_NO, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO, KC_NO,
+  KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
+  KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______, _______, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO
 ),
 
 /* Raise
  */
 [_RAISE] = LAYOUT_ortho_5x12(
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, _______, _______, _______,
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______,
+  _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______, _______, KC_RSFT, KC_RGUI, KC_RALT, KC_RCTL, _______,
+  _______, _______, _______, _______, _______, _______, KC_GRV,  KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+),
+
+/* Adjust
+ */
+[_ADJUST] = LAYOUT_ortho_5x12(
   _______, _______,  _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______,
   _______, _______, CG_NORM,  _______, _______, _______, _______, _______, _______, UG_SPDU, UG_SPDD, _______,
   _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
